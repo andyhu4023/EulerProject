08a2c3b1fc0d6c997862ffd924acfd11b1113b5c
humingan <andyhu2014@gmail.com>
2017-11-27 23:10:01 +0800

    update P4

--- 004.py
+++ 004.py
@@ -1,3 +1,9 @@
+'''
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+'''
+
 from Euler import palindromic
 from time import time
 
@@ -5,4 +11,4 @@ start = time()
 
 result = max(i*j for i in range(101, 1000) for j in range(i, 1000) if palindromic(str(i*j)))
 
-print('The answer is %d, it takes %f s.' %(result, time()-start))
+print('The answer is %d, it takes %f s.' % (result, time()-start))
